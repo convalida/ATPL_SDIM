cfe045950119ae0accf677c7317a13448c17c194
minakshisadana <minakshi.sadana@convalidatech.com>
2020-02-04 17:56:07 +0530

    Inventory commit upper half

--- app/src/main/java/com/example/atplsdim/InventoryIn.java
+++ app/src/main/java/com/example/atplsdim/InventoryIn.java
@@ -3,11 +3,21 @@ package com.example.atplsdim;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class InventoryIn extends AppCompatActivity {
+    Spinner warehouseSpinner,rackNoSpinner;
+    EditText uniqueId;
+    Button addId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,10 @@ public class InventoryIn extends AppCompatActivity {
             ActionBar actionBar = getSupportActionBar();
             actionBar.setTitle("Inventory In (Warehouse)");
         }
+        warehouseSpinner = findViewById(R.id.warehouseSpinner);
+        rackNoSpinner = findViewById(R.id.rackNumSpinner);
+        uniqueId = findViewById(R.id.uniqueId);
+        addId= findViewById(R.id.addBtn);
     }
 
     boolean doubleBackToExitPressedOnce = false;
@@ -35,5 +49,21 @@ public class InventoryIn extends AppCompatActivity {
                 }
             }, 2000);
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        SharedPreferences sharedPreferences = getSharedPreferences("Login",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+        finish();
+        Intent intent = new Intent(InventoryIn.this,Login.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
     }
 }
